9e93de1b567f8d426e568ccebfde4019e3f0235f
EdSamuelFM <72708772+EdSamuelFM@users.noreply.github.com>
2025-10-30 10:49:49 -0300

    Delete week01 directory

--- week01/scripts/getdates.js
+++
@@ -1,6 +0,0 @@
-// Get the current year for the copyright
-const currentYear = new Date().getFullYear();
-document.getElementById("currentyear").textContent = currentYear;
-
-// Get the last modified date of the document
-document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
